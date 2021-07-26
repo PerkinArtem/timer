10462ac7220690101a6b457b2329eee3405ba0ad
ape <artem.perkin1999@gmail.com>
2021-07-26 18:37:48 +0300

    timer done

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+const newYears = '1 Jan 2022';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysElement.innerText = days;
+    hoursElement.innerText = formatTime(hours);
+    minutesElement.innerText = formatTime(minutes);
+    secondsElement.innerText = formatTime(seconds);
+    
+    console.log(days, days, minutes, seconds)
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countdown();
+setInterval(countdown, 1000);
