1a04bda5279b857e96fd3bbd65ee89ff81a5e759
Artem <68643457+PerkinArtem@users.noreply.github.com>
2022-03-29 15:09:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
 
-const newYears = '1 Jan 2022';
+const newYears = '1 Jan 2023';
 
 function countdown() {
     const newYearsDate = new Date(newYears);
